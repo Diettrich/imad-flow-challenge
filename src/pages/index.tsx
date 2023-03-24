41f05802cdc0229d9babb6ef39d04003e74ccdc8
Imad <attif.imad@gmail.com>
2023-03-24 19:28:15 +0000

    [PAS-27] add getMonthlyAverageStockPrice procedure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const stocks = api.stock.getStocks.useQuery();
+  const monthlyAverageStockPrice = api.stock.getMonthlyAverageStockPrice.useQuery({ stockId: "clfmtwpk50000se7atjmy6avq" });
   return (
     <>
       <Head>
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
       <main>
         <h1>Flow App</h1>
         <pre>{JSON.stringify(stocks.data, null, 1)}</pre>
+        <pre>{JSON.stringify(monthlyAverageStockPrice.data, null, 1)}</pre>
       </main>
     </>
   );
--- src/server/api/routers/stock.ts
+++ src/server/api/routers/stock.ts
@@ -1,8 +1,19 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { getAveragePricePerMonth } from "./stock.helpers";
 
 export const stockRouter = createTRPCRouter({
   getStocks: publicProcedure.query(async ({ ctx }) =>
     ctx.prisma.stock.findMany()
   ),
-  getMonthlyAverageStockPrice: publicProcedure.query(() => null),
+  getMonthlyAverageStockPrice: publicProcedure
+    .input(z.object({ stockId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const dailyPriceRecords = await ctx.prisma.dailyPriceRecord.findMany({
+        where: {
+          stockId: input.stockId,
+        },
+      });
+      return getAveragePricePerMonth(dailyPriceRecords);
+    }),
 });
